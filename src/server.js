2a14313ea7b7fd69e639f7f673f0e267e3b53bd1
Harist Juliant <haristjulianto@gmail.com>
2022-08-10 05:26:22 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,10 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    // ini untuk development tutorial fase awal kita bedakan antara production dan develpoment
+    // karena di instance aws EC2, tidak dapat berjalan di localhost
+    // host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
